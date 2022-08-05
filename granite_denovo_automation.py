399293224cbbec58156834232a7735bab82a7954
Catherine <catherinejsong96@gmail.com>
2022-08-05 13:52:22 -0400

    fixing error

--- granite_denovo_automation.py
+++ granite_denovo_automation.py
@@ -207,22 +207,23 @@ def main():
 
         level_0 = list(temp_df.columns.get_level_values(0))
         if len([idx for idx, s in enumerate(column_list) if re.search(r"\d+", s)]) > 1:
-            print(len([idx for idx, s in enumerate(column_list) if re.search(r"\d+", s)]))
-        digit_index = [idx for idx, s in enumerate(column_list) if re.search(r"\d+", s)][0]
-        # sort the string columns first
-        string_columns = list(column_list[:digit_index])
-        string_columns.sort(reverse=True)
-        # sort non-string columns
-        digit_columns = list(column_list[digit_index:])
-        digit_columns.sort(key=num_sort)
+            digit_index = [idx for idx, s in enumerate(column_list) if re.search(r"\d+", s)][0]
+            # sort the string columns first
+            string_columns = list(column_list[:digit_index])
+            string_columns.sort(reverse=True)
+            # sort non-string columns
+            digit_columns = list(column_list[digit_index:])
+            digit_columns.sort(key=num_sort)
 
-        # getting the level 1 and level 0 columns
-        level_1 = string_columns + digit_columns
+            # getting the level 1 and level 0 columns
+            level_1 = string_columns + digit_columns
 
-        # create a new ordered column
-        new_column_tuple = [(level_0[i], level_1[i]) for i in range(0, len(level_0))]
-        multi_cols = pd.MultiIndex.from_tuples(new_column_tuple)
-        temp_df = pd.DataFrame(temp_df, columns=multi_cols)
+            # create a new ordered column
+            new_column_tuple = [(level_0[i], level_1[i]) for i in range(0, len(level_0))]
+            multi_cols = pd.MultiIndex.from_tuples(new_column_tuple)
+            temp_df = pd.DataFrame(temp_df, columns=multi_cols)
+        else:
+            temp_df = pd.DataFrame(temp_df, columns=level_0)
 
         # insert individual ID into the temporary dataframe
         identifier = [family_file.loc[family_file['File_ID'] == i].Individual_ID.values.item() for i in
