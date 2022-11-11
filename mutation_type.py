2828ab3c62e77d1037a4a5cee890be87fc029354
Catherine <catherinejsong96@gmail.com>
2022-11-11 17:50:00 -0500

    error fix

--- mutation_type.py
+++ mutation_type.py
@@ -30,28 +30,29 @@ for file in filename:
 
     #write header for file
     header = subprocess.check_output('gunzip -c ' + filename[0] + '| grep \'#CHROM\'', shell=True).splitlines()
-    header = header[0].decode('utf-8').split('\t')[:5]
-    exportfile.write(','.join(header))
-    exportfile.write('\n')
-
-    # extract element with novoPP >= 0.9 only from vcf files
-    if '.vcf.gz' in file:
-        mutation_list = subprocess.check_output('gunzip -c ' + file + '| grep \'novoPP=0.9\'', shell=True).splitlines()
-        # iterate through each mutation obtained from grep
-        for mutation in mutation_list:
-            # decode the into string and extract the mutation elements
-            mutation = mutation.decode('utf-8').split('\t')
-            mutation = mutation[:5]
-            print(mutation)
-            exportfile.write(','.join(mutation))
-            exportfile.write('\n')
-            mutation_change = ' > '.join(mutation[-2:])
-            if mutation_change in mutation_change_dict:
-                mutation_change_dict[mutation_change] += 1
-            else:
-                mutation_change_dict[mutation_change] = 1
-
+    if len(header) > 1:
+        header = header[0].decode('utf-8').split('\t')[:5]
+        exportfile.write(','.join(header))
         exportfile.write('\n')
+
+        # extract element with novoPP >= 0.9 only from vcf files
+        if '.vcf.gz' in file:
+            mutation_list = subprocess.check_output('gunzip -c ' + file + '| grep \'novoPP=0.9\'', shell=True).splitlines()
+            # iterate through each mutation obtained from grep
+            for mutation in mutation_list:
+                # decode the into string and extract the mutation elements
+                mutation = mutation.decode('utf-8').split('\t')
+                mutation = mutation[:5]
+                print(mutation)
+                exportfile.write(','.join(mutation))
+                exportfile.write('\n')
+                mutation_change = ' > '.join(mutation[-2:])
+                if mutation_change in mutation_change_dict:
+                    mutation_change_dict[mutation_change] += 1
+                else:
+                    mutation_change_dict[mutation_change] = 1
+
+            exportfile.write('\n')
 exportfile.close()
 
 
